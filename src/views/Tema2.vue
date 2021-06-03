597772117b61744c91c8ea87e2627c6cd8eb265e
maronita <maronita@users.noreply.github.com>
2021-06-03 18:03:59 -0500

    img

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -16,7 +16,7 @@
             br
             p Una obligación (contrato) que será cancelado por la entidad entregando o recibiendo una cantidad acordada de sus instrumentos de patrimonio propio a cambio de un importe fijo en efectivo o cualquier otro activo financiero será un instrumento de patrimonio.
           .col-lg-6
-            img.img-a.my-3(src="@/assets/curso/images/tema2/img_9.svg" alt="", style="width:500px; display:block; margin:auto 0;").img-fluid
+            img.img-a.my-3(src="@/assets/curso/images/tema2/img_9.svg" alt="", style="width:500px; display:block; margin:auto 0;")
 
     #t_2_1.titulo-segundo
       .h4 2.1 Reconocimiento, medición, presentación, Revelación:
@@ -29,27 +29,27 @@
     
     .row.justify-content-center.align-items-center.mt-5
       .col-lg-6
-          img.img-a.my-3(src="@/assets/curso/images/tema2/img_10.svg" alt="", style="width:500px; display:block; margin:auto 0;").img-fluid
+          img.img-a.my-3(src="@/assets/curso/images/tema2/img_10.svg" alt="", style="width:500px; display:block; margin:auto 0;")
       .col-lg-6
         hr.mt-5.mb-4
         .row.justify-content-center.align-items-center
           .col-auto
             figure
-              img.me-5(src='@/assets/curso/images/tema2/ico_1.svg' alt="", style="width:70px; display:block; margin:auto 0;").img-float     
+              img.me-5(src='@/assets/curso/images/tema2/ico_1.svg' alt="", style="width:70px; display:block; margin:auto 0;")    
           .col
             p Si los instrumentos de patrimonio se emiten antes de que la entidad reciba el efectivo u otros recursos, la entidad presentará el importe por cobrar como una compensación al patrimonio en su estado de situación financiera, no como un activo.
         hr.mt-5.mb-4
         .row.justify-content-center.align-items-center
           .col-auto
             figure
-              img.me-5(src='@/assets/curso/images/tema2/ico_2.svg' alt="", style="width:70px; display:block; margin:auto 0;").img-float      
+              img.me-5(src='@/assets/curso/images/tema2/ico_2.svg' alt="", style="width:70px; display:block; margin:auto 0;")     
           .col
             p Si la entidad recibe el efectivo u otros recursos antes de que se emitan los instrumentos de patrimonio, y no se puede requerir a la entidad el reembolso del efectivo o de los otros recursos recibidos, la entidad reconocerá el correspondiente incremento en el patrimonio en la medida de la contraprestación recibida. 
         hr.mt-5.mb-4
         .row.justify-content-center.align-items-center
           .col-auto
             figure
-              img.me-5(src='@/assets/curso/images/tema2/ico_3.svg' alt="", style="width:70px; display:block; margin:auto 0;").img-float     
+              img.me-5(src='@/assets/curso/images/tema2/ico_3.svg' alt="", style="width:70px; display:block; margin:auto 0;")
           .col
             p En la medida en que los instrumentos de patrimonio hayan sido suscritos, pero no emitidos y la entidad no haya todavía recibido el efectivo o los otros recursos, la entidad no reconocerá un incremento en el patrimonio.
         hr.mt-5.mb-4
@@ -64,7 +64,7 @@
             .h5 Ejemplo:
             p La sociedad Contable SAS, compra el primero de julio del año 0 un paquete de acciones del Banco de Bogotá, por valor de $130.000.000 y las clasifica como activos disponibles para la venta.
         .col-lg-6
-          img.img-jumbotron-padding(src='@/assets/curso/images/tema2/img_13.svg' alt="", style="width:500px; display:block; margin:auto 0;").img-float
+          img.img-jumbotron-padding(src='@/assets/curso/images/tema2/img_13.svg' alt="", style="width:500px; display:block; margin:auto 0;")
 
     p.mt-3 La sociedad Contable SAS, compra el primero de julio del año 0 un paquete de acciones del Banco de Bogotá, por valor de $130.000.000 y las clasifica como activos disponibles para la venta.
     br
@@ -93,7 +93,7 @@
             .h5 Ejemplo:
             p La sociedad Contable SAS, compra el primero de julio del año 0 un paquete de acciones del Banco de Bogotá, por valor de $130.000.000 y las clasifica como activos disponibles para la venta.
       .col-lg-6
-        img.img-jumbotron-padding(src='@/assets/curso/images/tema2/img_11.svg' alt="", style="width:500px; display:block; margin:auto 0;").img-float
+        img.img-jumbotron-padding(src='@/assets/curso/images/tema2/img_11.svg' alt="", style="width:500px; display:block; margin:auto 0;")
       
       p.mt-5 Reconocimiento Inicial
 
@@ -301,10 +301,10 @@
     .row.justify-content-center.align-items-center.mb-5.mt-5
       .col-lg-6
         h6 Ejemplo: Presentación ORI
-        img.img(src='@/assets/curso/images/tema2/ico_7.svg' alt="", style="width:400px; display:block; margin:auto 0;").img-float
+        img.img(src='@/assets/curso/images/tema2/ico_7.svg' alt="", style="width:400px; display:block; margin:auto 0;")
       .col-lg-6
         h6 Otro resultado Integral
-        img.img(src='@/assets/curso/images/tema2/ico_8.svg' alt="", style="width:600px; display:block; margin:auto 0;").img-float            
+        img.img(src='@/assets/curso/images/tema2/ico_8.svg' alt="", style="width:600px; display:block; margin:auto 0;")      
 
     p.mt-5 Para ampliar la información sobre la normatividad aplicable a instrumentos financieros, los invito a revisar los siguientes enlaces:
 
