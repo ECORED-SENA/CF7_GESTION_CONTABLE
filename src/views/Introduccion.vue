51738389a81d61e00a71581a76b46040af963d6c
maronita <maronita@users.noreply.github.com>
2021-06-18 19:46:57 -0500

    intro

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -9,11 +9,14 @@
           i.fas.fa-info
       .h3 Introducción
     
-    figure.mb-5
-      .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-      figcaption Video: Leyenda del video
-    
+    .row.justify-content-center.align-items-center.mb-5
+        .col-auto
+            img(src="@/assets/curso/images/tema1/intro.svg" alt="", style="width:700px; display:inline-block; margin: 0 auto;")
+
+    p.mb-3 En el presente componente se abordan los temas: activos financieros, pasivos financieros, instrumentos de patrimonio y la normatividad vigente aplicable al presente tema. Los instrumentos financieros son contratos que dan lugar, simultáneamente, a un activo financiero en una entidad y a un pasivo financiero o a un instrumento de patrimonio en otra entidad, lo que permite concretar todos estos conceptos en el momento de reconocer, medir y revelar de manera correcta los diferentes títulos que se comercian a diario en el mercado de capitales y que forman parte del día a día de cualquier empresa en sus procesos de maximizar la rentabilidad de sus activos y de financiación de su negocio. 
+
+    p.mb-3 Nota aclaratoria: las tablas que se toman como referencia en los ejemplos de cada uno de los temas desarrollados, elaboradas mediante el programa Excel son derivadas de estos ejercicios, la elaboración es propia del autor del presente componente, por lo tanto, no se numeran y no se establece un nombre específico.
+
    
 
 </template>
